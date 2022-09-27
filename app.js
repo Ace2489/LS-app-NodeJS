3d6ddd6aede18705380b2ecd2b68ce338c4dc595
Covenant Okoroafor <okoroaforcovenant99@gmail.com>
2022-09-27 16:42:40 +0100

    Add blue highlighting for folders

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const folder = './';
-const fs = require('fs');
+import fs from 'fs'
+import chalk from 'chalk'
 
 fs.readdir(folder, (err,files) => {
     files.forEach((file)=>{
-        process.stdout.write(`${file}  `);
+        if (fs.statSync(`./${file}`).isDirectory()){//Check if each item returned in the directory is a folder
+            process.stdout.write(chalk.blue.bold(`${file}  `));
+        }
+        else{
+            process.stdout.write(`${file}  `);
+        }
     })
     process.stdout.write('\n')                          
 })
